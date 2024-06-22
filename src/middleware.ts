034a0e7b656dd83524baae67e80f6a27d017bcd4
g-120902 <gregoryalbert1209@gmail.com>
2024-06-22 20:49:07 +0400

    finished website v1

--- src/middleware.ts
+++
@@ -1,7 +0,0 @@
-"use client";
-import { stackMiddlewares } from './middlewares/stackMiddlewares';
-// import { authMiddleware } from './middlewares/authMiddleware';
-import { langMiddleware } from './middlewares/langMiddleware';
-
-const middlewares = [langMiddleware];
-export default stackMiddlewares(middlewares);
